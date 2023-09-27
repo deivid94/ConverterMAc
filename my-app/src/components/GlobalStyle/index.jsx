48bedc371ab47f8e046d87d571eaf18eeca5ae16
deivid94 <md031194@gmail.com>
2023-09-26 23:50:02 -0300

    refactory app styled components

--- my-app/src/components/GlobalStyle/index.jsx
+++ my-app/src/components/GlobalStyle/index.jsx
@@ -1,9 +1,15 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle `
-@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@700&display=swap');
-font-family: 'Fira Code', monospace;
+    display: flex;
+    justify-content: center;
+    font-family : 'Roboto';
+    flex-direction: column;
+    align-items: center;
+
+    
 
 `
+
 
 export default GlobalStyle
--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,17 +1,25 @@
-import GlobalStyle from "@/components/GlobalStyle"
-import ContainerApp from "@/components/ContainerApp"
-import Titulo from "@/components/Titulo"
-import InputMac from "@/components/InputMac"
+import styled from "styled-components"
+import Titulo from '../components/Titulo'
+import InputMac from '../components/InputMac'
+import GlobalStyle from '../components/GlobalStyle'
 
+const AppContainer = styled.div `
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+   height: 100vh;
+   background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+
+`
 
 export default function App (){
   return (
-    <ContainerApp>
+    <AppContainer>
       <GlobalStyle />
       <Titulo>Converter o MAC</Titulo>
-
-      <InputMac className="dataSearch" type="text" name="" id="" placeholder="Digite o mac address" />
-    </ContainerApp>
+      <InputMac type="text" name="" id="" placeholder="Digite o mac address" />
+    </AppContainer>
 
   )
 }
