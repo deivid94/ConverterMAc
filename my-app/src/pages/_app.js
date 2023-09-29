881b7ebde640cc94889e7ca220f6f6e6d70b42d7
deivid94 <md031194@gmail.com>
2023-09-29 01:03:09 -0300

    Criando funcao que remove dois pontos dos MAC

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import Titulo from '../components/Titulo'
 import InputMac from '../components/InputMac'
 import GlobalStyle from '../components/GlobalStyle'
+import { useState } from "react"
 
 const AppContainer = styled.div `
   display:flex;
@@ -13,17 +14,30 @@ const AppContainer = styled.div `
 
 `
 
+
 export default function App (){
+   const [MacDigitado, setMacDigitado] = useState ('')
+
+    function converterMac (MacDigitado){
+      let macConvertido = MacDigitado.replace(":", " ")
+      console.log(macConvertido)
+    }
+
+
   return (
     <AppContainer>
       <GlobalStyle />
       <Titulo>Converter o MAC</Titulo>
       <InputMac 
       placeholder="Digite o mac address"
-      onBlur={event =>console.log(event.target.value)}
+      onBlur={event =>converterMac (event.target.value)}
+
+        
+      
       />
       
     </AppContainer>
-
+    
   )
+  
 }
